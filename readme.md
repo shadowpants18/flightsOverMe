855ffcb8bde857a97815b91c4e0ca7de5564da87
Joshua Lee <jminlee24@outlook.com>
2021-01-21 22:48:51 -0500

    added radius and center functions

--- readme.md
+++ readme.md
@@ -8,11 +8,11 @@ EDIT CONFIG.JSON AND FILL IN THE VALUES NEEDED
 setBoundaries: default = true, set to false if you want specify a center coordinate and a radius rather than boundaries.   
   
 If setBoundaries is true:  
-  lamin: set to the minimum latitude to detect  
-  lamax: set to the maximum latitude to detect  
-  lomin: set to the minimum longitude to detect  
-  lomax: set to the maximum longitude to detect  
+lamin: set to the minimum latitude to detect  
+lamax: set to the maximum latitude to detect  
+lomin: set to the minimum longitude to detect  
+lomax: set to the maximum longitude to detect  
   
 If setBoundaries is false:  
-  latlong: list containing the latitude and longitude [lat, long]  
-  radius: radius in kilometers(max of 1000)  
+latlong: list containing the latitude and longitude [lat, long]  
+radius: radius in kilometers(max of 1000)  
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const fetch = require('node-fetch');
 const config = require('./config.json')
+
 const url = 'https://opensky-network.org/api/states/all?'
+
 let params
+
+function calculateDistance(radius, lat, long){
+  let latDif = radius/110.574
+  let lat1 = lat - latDif
+  let lat2 = lat + latDif
+  let longDif = radius / (111.320 * Math.cos(latDif * Math.PI/180))
+  let long1 = long - longDif 
+  let long2 = long + longDif
+  return [lat1, long1, lat2, long2]
+}
 if(config.setBoundaries == true){
   params = new URLSearchParams({ 
     lamin: config.lamin,
@@ -10,10 +22,16 @@ if(config.setBoundaries == true){
     lomax: config.lomax
   })
 }else{
-  let lamin, lomin, lamax, lomax
   let [lat, long] = config.latlong
-}
+  let [lamin, lomin, lamax, lomax] = calculateDistance(config.radius, lat, long)
 
+  params = new URLSearchParams({ 
+    lamin: String(lamin),
+    lomin: String(lomin),
+    lamax: String(lamax),
+    lomax: String(lomax)
+  })
+}
 
 async function main(run){
   while(true){
@@ -22,11 +40,11 @@ async function main(run){
       fetch(url + params).then(res=>{
         res.json().then(json=>{
           console.log(json, "\nnew line")
+          return json
         })
       })
     }catch(e){
       console.error(e)
-      break
     }
   }
 }
