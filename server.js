3dbaaf26e75c29d2109f0fd8a44d9662fb9ff388
Joshua Lee <jminlee24@outlook.com>
2021-01-22 00:58:59 -0500

    added tweets

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const fetch = require('node-fetch');
 const config = require('./config.json')
+const Twitter = require('twitter')
 
-const url = 'https://opensky-network.org/api/states/all?'
+const date = new Date()
+const twitClient = new Twitter({
+  consumer_key:config.twitterKeys.consumer_key,
+  consumer_secret:config.twitterKeys.consumer_secret,
+  access_token_key:config.twitterKeys.access_token_key,
+  access_token_secret:config.twitterKeys.access_token_secret
+})
 
 let params
-
+const url = 'https://opensky-network.org/api/states/all?'
 function calculateDistance(radius, lat, long){
   let latDif = radius/110.574
   let lat1 = lat - latDif
@@ -14,6 +21,35 @@ function calculateDistance(radius, lat, long){
   let long2 = long + longDif
   return [lat1, long1, lat2, long2]
 }
+
+function TweetTimed(number, time){
+  let message = `${number} planes have flown over ${config.locationName} in the last ${time}.`
+  twitClient.post('statuses/update', {status: message}).then(tweet=>{
+    console.log(tweet)
+  })
+  .catch(e=>{
+    throw e
+  })
+}
+
+function TweetFlight(plane){
+  let message = `#${plane[1]} : A plane has just flown over ${config.locationName} and originated from ${plane[2]}.`
+  if(plane[13] != null){
+    message += `\nGeometric altitude: ${plane[13]} m/s`
+  }
+  if(plane[9] != null){
+    message += `\nVelocity: ${plane[9]} m/s`
+  }
+  if(plane[5] != null && plane[6] != null){
+    message += `\nLatitude and longitude: ${plane[6]}, ${plane[5]}`
+  }
+  twitClient.post('statuses/update', {status: message}).then(tweet=>{
+    console.log(tweet)
+  }).catch(e=>{
+    throw e
+  })
+}
+
 if(config.setBoundaries == true){
   params = new URLSearchParams({ 
     lamin: config.lamin,
@@ -33,18 +69,49 @@ if(config.setBoundaries == true){
   })
 }
 
+let planesList = []
+let hourCount = 0
+let dateCount = 0
+
 async function main(run){
+  let hour = date.getHours()
+  let dateOfMonth = date.getDate()
   while(true){
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    await new Promise(resolve => setTimeout(resolve, 5 * 1000));
     try{
       fetch(url + params).then(res=>{
-        res.json().then(json=>{
-          console.log(json, "\nnew line")
-          return json
+        res.json().then(data=>{
+          let json = data['states']
+          console.log(data)
+          if(data['states'] == null) {
+            console.log('no planes')
+            return
+          }
+          if(hour != date.getHours()){
+            console.log("Hourly count " + hourCount)
+            TweetTimed(hourCount, "hour")
+            hour = date.getHours()
+            hourCount = 0
+
+          }
+          if(dateOfMonth != date.getDate()){
+            dateOfMonth = date.getDate()
+            console.log("Daily count " + dateCount)
+            TweetTimed(dateCount, "day")
+            dateCount = 0
+          }
+          for(let plane of json){
+            if(!planesList.includes(plane[0]) && plane[8] == false){
+              planesList.push(plane[0])
+              hourCount ++
+              TweetFlight(plane)
+            }
+          }
         })
       })
     }catch(e){
       console.error(e)
+      break
     }
   }
 }
